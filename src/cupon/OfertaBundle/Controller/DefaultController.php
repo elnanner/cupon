5ae65fdd00924e714caa16e04ef3631e6319e755
Luciano <elnanner2@gmail.com>
2017-01-15 13:21:33 -0300

    primer bundle

---
+++ src/cupon/OfertaBundle/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace cupon\OfertaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    
+    public function ayudaAction()
+    {
+        return new Response("Ayuda");
+    }
+}
